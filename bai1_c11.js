4e7dd2cce12379d8277a83d7d5ee9d9ada5fe59d
Tu Nguyen <nguyenvantuha123@gmail.com>
2023-02-10 21:16:59 +0700

    Fix Bai 1 Chuong 11

--- bai1_c11.js
+++ bai1_c11.js
@@ -36,31 +36,60 @@ var students = [
     },
 ];
 
-const menu = `
-1. Nhap diem cho sinh vien 
-2. Hien thị danh sach sinh vien
+// const menu = `
+// 1. Nhap diem cho sinh vien
+// 2. Hien thị danh sach sinh vien
 
-Nhap lua chon: `;
+// Nhap lua chon: `;
 
-let enter = Number(prompt(menu));
+// let enter = Number(prompt(menu));
 
-while (enter >= 1 && enter <= 2) {
-    if (enter === 1) {
-        nhapDiemSV();
-    } else if (enter === 2) {
-        hienThiDanhSachSV();
-    }
-    enter = Number(prompt(menu));
-}
+// while (enter >= 1 && enter <= 2) {
+//     if (enter === 1) {
+//         nhapDiemSV();
+//     } else if (enter === 2) {
+//         hienThiDanhSachSV();
+//     }
+//     enter = Number(prompt(menu));
+// }
 
-function nhapDiemSV() {
-    students.forEach(function (obj) {
-        obj.diemToan = Number(
-            prompt("Nhap diem toan cua SV " + obj.name + " :")
-        );
-        obj.diemLy = Number(prompt("Nhap diem ly cua SV " + obj.name + " :"));
-        obj.diemHoa = Number(prompt("Nhap diem hoa cua SV " + obj.name + " :"));
-    });
+// function nhapDiemSV() {
+//     students.forEach(function (obj) {
+//         obj.diemToan = Number(
+//             prompt("Nhap diem toan cua SV " + obj.name + " :")
+//         );
+//         obj.diemLy = Number(prompt("Nhap diem ly cua SV " + obj.name + " :"));
+//         obj.diemHoa = Number(prompt("Nhap diem hoa cua SV " + obj.name + " :"));
+//     });
+// }
+
+// function hienThiDanhSachSV() {
+//     console.log("=== DANH SACH SINH VIEN ===");
+//     students.forEach(function (obj) {
+//         console.log("id: " + obj.id);
+//         console.log("name: " + obj.name);
+//         console.log("address: " + obj.address);
+//         console.log("toan: " + obj.diemToan);
+//         console.log("ly: " + obj.diemLy);
+//         console.log("hoa: " + obj.diemHoa);
+//         if (obj.id < students.length) {
+//             console.log("===========");
+//         }
+//     });
+// }
+
+students.forEach(function (obj) {
+    obj.toan = randomNumberInRange();
+    obj.ly = randomNumberInRange();
+    obj.hoa = randomNumberInRange();
+});
+
+hienThiDanhSachSV();
+
+function randomNumberInRange(a = 0, b = 10) {
+    if (a >= b) return -1;
+    const random = Math.random() * (b - a);
+    return Math.round(random) + a;
 }
 
 function hienThiDanhSachSV() {
@@ -69,9 +98,9 @@ function hienThiDanhSachSV() {
         console.log("id: " + obj.id);
         console.log("name: " + obj.name);
         console.log("address: " + obj.address);
-        console.log("toan: " + obj.diemToan);
-        console.log("ly: " + obj.diemLy);
-        console.log("hoa: " + obj.diemHoa);
+        console.log("toan: " + obj.toan);
+        console.log("ly: " + obj.ly);
+        console.log("hoa: " + obj.hoa);
         if (obj.id < students.length) {
             console.log("===========");
         }
