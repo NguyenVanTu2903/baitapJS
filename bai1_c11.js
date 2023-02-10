1a6a09dcb7b4e2cb64ee8e2d012aa17af6e19712
Tu Nguyen <nguyenvantuha123@gmail.com>
2023-02-10 19:25:14 +0700

    Bai1_Chuong11

---
+++ bai1_c11.js
@@ -0,0 +1,79 @@
+var students = [
+    {
+        id: 1,
+        name: "Dinh",
+        address: "hue",
+    },
+    {
+        id: 2,
+        name: "Nam",
+        address: "quang nam",
+    },
+    {
+        id: 3,
+        name: "Tan",
+        address: "da nang",
+    },
+    {
+        id: 4,
+        name: "Hung",
+        address: "hue",
+    },
+    {
+        id: 5,
+        name: "Tri",
+        address: "quang tri",
+    },
+    {
+        id: 6,
+        name: "Anh",
+        address: "hue",
+    },
+    {
+        id: 7,
+        name: "Binh",
+        address: "da nang",
+    },
+];
+
+const menu = `
+1. Nhap diem cho sinh vien 
+2. Hien thị danh sach sinh vien
+
+Nhap lua chon: `;
+
+let enter = Number(prompt(menu));
+
+while (enter >= 1 && enter <= 2) {
+    if (enter === 1) {
+        nhapDiemSV();
+    } else if (enter === 2) {
+        hienThiDanhSachSV();
+    }
+    enter = Number(prompt(menu));
+}
+
+function nhapDiemSV() {
+    students.forEach(function (obj) {
+        obj.diemToan = Number(
+            prompt("Nhap diem toan cua SV " + obj.name + " :")
+        );
+        obj.diemLy = Number(prompt("Nhap diem ly cua SV " + obj.name + " :"));
+        obj.diemHoa = Number(prompt("Nhap diem hoa cua SV " + obj.name + " :"));
+    });
+}
+
+function hienThiDanhSachSV() {
+    console.log("=== DANH SACH SINH VIEN ===");
+    students.forEach(function (obj) {
+        console.log("id: " + obj.id);
+        console.log("name: " + obj.name);
+        console.log("address: " + obj.address);
+        console.log("toan: " + obj.diemToan);
+        console.log("ly: " + obj.diemLy);
+        console.log("hoa: " + obj.diemHoa);
+        if (obj.id < students.length) {
+            console.log("===========");
+        }
+    });
+}
